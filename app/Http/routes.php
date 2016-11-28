b0712051f6f04d63b9a2baae54d813bf4e433e5e
ArteriusDayne <zakafein@hotmail.com>
2016-11-28 11:18:29 -0500

    Routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -10,28 +9,18 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
 Route::get('/','PagesController@welcome');
-
-/*** User Routes ***/
-Route::get('/login', ['as' => 'login', 'uses' => 'AuthController@login']);
-Route::post('/handleLogin', ['as' => 'handleLogin', 'uses' => 'AuthController@handleLogin']);
-Route::get('/home', ['middleware' => 'auth', 'as' => 'home', 'uses' => 'UsersController@home']);
-Route::get('/logout', ['as' => 'logout', 'uses' => 'AuthController@logout']);
-Route::get('/create', ['as' => 'create', 'uses' => 'UsersController@create']);
-Route::resource('users', 'UsersController', ['only' => ['create', 'store']]);
-
-
-Route::get('/new-sales', ['as' => 'sales', 'uses' => 'SalesController@create']);
-Route::resource('sales', 'SalesController', ['only' => ['create', 'store']]);
-
-/*** Car Routes ***/
-Route::resource('cars', 'CarsController');
-Route::get('/inventory','CarsController@index');
-	
-/*** Static Page Routes ***/
-Route::get('/schedule','PagesController@schedule');
-Route::get('/contact','PagesController@contact');
-Route::get('/about','PagesController@about');
-Route::get('/feedback','PagesController@feedback');
-Route::get('/subscribe','PagesController@subscribe');
+	Route::get('/login', ['as' => 'login', 'uses' => 'AuthController@login']);
+	Route::post('/handleLogin', ['as' => 'handleLogin', 'uses' => 'AuthController@handleLogin']);
+	Route::get('/home', ['middleware' => 'auth', 'as' => 'home', 'uses' => 'UsersController@home']);
+	Route::get('/logout', ['as' => 'logout', 'uses' => 'AuthController@logout']);
+	Route::get('/create', ['as' => 'create', 'uses' => 'UsersController@create']);
+	Route::resource('users', 'UsersController', ['only' => ['create', 'store']]);
+	Route::get('/new-sales', ['as' => 'sales', 'uses' => 'SalesController@create']);
+	Route::resource('sales', 'SalesController', ['only' => ['create', 'store']]);
+	Route::get('/inventory','PagesController@inventory');
+	Route::get('/schedule','PagesController@schedule');
+	Route::get('/contact','PagesController@contact');
+	Route::get('/about','PagesController@about');
+	Route::get('/feedback','PagesController@feedback');
+	Route::get('/subscribe','PagesController@subscribe');
