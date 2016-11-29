b507f19f47ab7ddc41d1195e22500f0cb33e7d7b
KumTou <kumtou.lam@gmail.com>
2016-11-29 12:39:57 -0500

    replace fillable variables with tel and name

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'email', 'password', 'firstName', 'lastName'
+        'email', 'password', 'name', 'telephone'
     ];
 
     /**
