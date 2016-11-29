03fad19e3a3f2f112be0c40defab80dcf2512658
ses10 <sesma@live.com>
2016-11-28 19:30:03 -0500

    add user roll seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
          $this->call(UserTableSeeder::class);
          $this->call(PetTableSeeder::class);
          $this->call(RoleTableSeeder::class);
+         $this->call(RoleUserTableSeeder::class);
     }
 }
