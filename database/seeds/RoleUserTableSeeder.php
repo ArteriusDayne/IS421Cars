5c152b01f38c3bbc165ac622543941340c7bf455
ses10 <sesma@live.com>
2016-12-03 09:41:13 -0500

    add role to admin in seeder

--- database/seeds/RoleUserTableSeeder.php
+++ database/seeds/RoleUserTableSeeder.php
@@ -12,6 +12,7 @@ class RoleUserTableSeeder extends Seeder
     public function run()
     {
         App\User::find(2)->attachRole(App\Role::find(1));
+        App\User::find(2)->attachRole(App\Role::find(2));
         App\User::find(1)->attachRole(App\Role::find(2));
     }
 }
