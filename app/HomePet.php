293012ef7e81608ad7b915f1ede80ab37cf3a79c
ses10 <sesma@live.com>
2016-12-08 18:27:14 -0500

    get pets for carousel

--- app/HomePet.php
+++ app/HomePet.php
@@ -3,9 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use DB;
 
 class HomePet extends Model
 {
     protected $table = 'home_pets';
     public $timestamps = false;
+
+    //return base64 images of pets on front page carousel
+    public static function getCarouselPets()
+    {
+    	return 
+    	DB::table('home_pets')->join('pets', 'pets.id', '=', 'home_pets.pet_id')->select('pets.name', 'home_pets.caption', 'pets.image')->where('home_pets.location', '=', 'carousel')->get();
+    }
 }
