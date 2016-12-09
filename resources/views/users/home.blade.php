ffeb152f75534560f5b73af5c77fc79ace011226
ses10 <sesma@live.com>
2016-12-08 20:11:49 -0500

    link to manage HomePets

--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -1,15 +1,21 @@
 
 @extends('layouts.master')
+
 @section('page_content')
 <h3 style="text-align:center">Welcome, {{ $name }}</h3>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 @role('admin')
-<br>
-@include('admin.users')
+    <br>
+    @include('admin.users')
+    <a href="/homepets">View Pets on Homepage</a><br>
 @endrole
 
+@role('provider')
+    <a href="/pets/create">Add new pet for Adoption</a><br>
+@endrole
 
 <a href="#">Request to be a Provider!</a><br>
 @if( !\Auth::user()->hasRole('adopter') )
@@ -41,8 +47,6 @@
 </tbody>
 
 @endif
-
-<a href="/pets/create">Add new pet for Adoption</a><br>
 
 
 <a><center>Current Pets</center></a>
