256d7cdbff2ffb426f7789a297ac2ea9cdb0d67a
KumTou <kumtou.lam@gmail.com>
2016-12-13 10:06:59 -0500

    revert img size changes

--- resources/views/pets/index.blade.php
+++ resources/views/pets/index.blade.php
@@ -5,17 +5,18 @@
     <div class="card-deck">
 		<div class="row">
 			@foreach($pets as $pet)
-				<div class ="col-md-3" style="padding:0px;">
+				<div class ="col-md-3" style="padding:0;">
 						<div class="card">
-						<a href = "/details">
-							<img class="card-img-top img" style= "width:500px; height:200px; object-fit:cover; overflow:hidden;"src="{{ asset('img/animals/providerUpload/').'/'. $pet['image'] }}" alt="No Animal Photos"> 
+
+							<img class="card-img-top img" src="{{ $pet['image'] }}" alt="Pet Image">
+
 							<div class="card-block">
 								<h4 class="card-title">{{ $pet['name'] }}</h4>
 								<p class="text-muted text-center">
 								{{ $pet['description'] }}
 								<br>
 								Location: {{ $pet['location'] }}</p>
-								<a href="/details">View more details</a></a>
+								<a href="{{ action('PetsController@show', ['id' => $pet['id']]) }}">View more details</a>
 							</div>
 						</div>
 						<br>
