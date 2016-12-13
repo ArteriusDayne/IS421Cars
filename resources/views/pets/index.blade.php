acfc0675c19f67eb4aba485f812f21030cbbdf39
KumTou <kumtou.lam@gmail.com>
2016-12-13 00:57:08 -0500

    equal img size on cards

--- resources/views/pets/index.blade.php
+++ resources/views/pets/index.blade.php
@@ -5,18 +5,17 @@
     <div class="card-deck">
 		<div class="row">
 			@foreach($pets as $pet)
-				<div class ="col-md-3" style="padding:0;">
+				<div class ="col-md-3" style="padding:0px;">
 						<div class="card">
-
-							<img class="card-img-top img" src="{{ $pet['image'] }}" alt="Pet Image">
-
+						<a href = "/details">
+							<img class="card-img-top img" style= "width:500px; height:200px; object-fit:cover; overflow:hidden;"src="{{ asset('img/animals/providerUpload/').'/'. $pet['image'] }}" alt="No Animal Photos"> 
 							<div class="card-block">
 								<h4 class="card-title">{{ $pet['name'] }}</h4>
 								<p class="text-muted text-center">
 								{{ $pet['description'] }}
 								<br>
 								Location: {{ $pet['location'] }}</p>
-								<a href="{{ action('PetsController@show', ['id' => $pet['id']]) }}">View more details</a>
+								<a href="/details">View more details</a></a>
 							</div>
 						</div>
 						<br>
