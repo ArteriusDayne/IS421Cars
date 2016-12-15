e5e4a0c765a0ce1687bc7d32dea0e07b175d469a
Max A <mla23@njit.edu>
2016-12-14 22:46:12 -0500

    Update 2015_09_09_234428_create_calendar_events_table.php

--- database/migrations/2015_09_09_234428_create_calendar_events_table.php
+++ database/migrations/2015_09_09_234428_create_calendar_events_table.php
@@ -10,16 +10,30 @@ class CreateCalendarEventsTable extends Migration {
 	 *
 	 * @return void
 	 */
+
+	//STEP 1: LOG In To psql (psql template1)
+	//STEP 2: DROP Table calendar_events;
+	//STEP 3: RUN sql below to create new table
+	//create table calendar_events(id int primary key not null,
+	// name char(100),description char(100), location char(100),eventstart timestamp,
+	// eventend timestamp,created_at timestamp, updated_at timestamp );
+	//STEP 4: Get out of psql
+	//STEP 5: php artisan db:seed --class=CalendarEventTableSeeder
+
+
+
 	public function up()
 	{
+
+
 		Schema::create('calendar_events', function(Blueprint $table) {
-            $table->increments('id');
-            $table->string('title');
-            $table->dateTime('start');
-            $table->dateTime('end');
-            $table->boolean('is_all_day');
-            $table->string('background_color')->nullable();
-            $table->timestamps();
+			$table->increments('id');
+			$table->string('name',100);
+			$table->string('description',100);
+			$table->string('location',100);
+			$table->timestamp('eventstart');
+			$table->timestamp('eventend');
+
         });
 	}
 
