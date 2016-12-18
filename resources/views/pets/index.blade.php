a7d609183526481e29d8f3c656160e69be1d99d7
KumTou <kumtou.lam@gmail.com>
2016-12-18 16:06:28 -0500

    equal image size round 2
    add details link to img
    limit lines of description

--- resources/views/pets/index.blade.php
+++ resources/views/pets/index.blade.php
@@ -1,20 +1,25 @@
 @extends('layouts.master')
 @section('page_content')
+
 <div class="card-deck-wrapper">
     <div class="card-deck">
 		<div class="row">
 			@foreach($pets as $pet)
 				<div class ="col-md-3" style="padding:0;">
 						<div class="card">
-
-							<img class="card-img-top img" src="{{ $pet['image'] }}" alt="Pet Image">
-
+							
+							<a href="{{ action('PetsController@show', ['id' => $pet['id']]) }}">
+							<img class="card-img-top img" src="{{ $pet['image'] }}" alt="Pet Image" style= "width:100%; height:10em; object-fit:cover; overflow:hidden;"
+							</a>
+							
 							<div class="card-block">
 								<h4 class="card-title">{{ $pet['name'] }}</h4>
-								<p class="text-muted text-center">
-								{{ $pet['description'] }}
-								<br>
+								<p>
 								Location: {{ $pet['location'] }}</p>
+								<p class="text-muted text-center"style="height:1.875em;  line-height:0.9375em; overflow:hidden;
+								text-overflow:ellipsis;">
+								{{ $pet['description'] }}
+								</p>
 								<a href="{{ action('PetsController@show', ['id' => $pet['id']]) }}">View more details</a>
 							</div>
 						</div>
